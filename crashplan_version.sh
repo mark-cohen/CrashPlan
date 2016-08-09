230fe85a9a29a6525ff92c1a204a9a594b68461e
mark-cohen <veberfetzel@gmail.com>
2016-08-08 21:27:40 -0700

    Update crashplan_version.sh

--- crashplan_version.sh
+++ crashplan_version.sh
@@ -6,7 +6,7 @@
 
 ### Requires CrashPlan Pro: https://www.crashplan.com/en-us/business/
 
-# If CrashPlan is installed, report version
+# If CrashPlan client is installed, report version
 if [ -f "/Applications/CrashPlan.app/Contents/Info.plist" ] ; then
 	CLIENT_VERSION=$( defaults read "/Applications/CrashPlan.app/Contents/Info.plist" CFBundleShortVersionString )
 	echo "CrashPlan client version: $CLIENT_VERSION"
@@ -14,7 +14,7 @@ else
 	echo "CrashPlan client not installed"
 fi
 
-# If server present, report version
+# If CrashPlan server is present, report version
 if [ -f "/Applications/PROServer.app/Contents/Info.plist" ] ; then
     SERVER_VERSION=$( defaults read "/Applications/PROServer.app/Contents/Info.plist" CFBundleShortVersionString )
 	echo "CrashPlan server version: $SERVER_VERSION"
